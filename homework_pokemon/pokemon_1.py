69efa521c07341b7543f377a8a5d3ef0316d841f
Artem Prikhodchenko <artmwow1931@gmail.com>
2024-06-09 17:40:18 +0300

    succesfull

---
+++ homework_pokemon/pokemon_1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
+
+# print(pd.get_dummies(data['whoAmI']).astype(int))
+
+data['data_frame'] = 1
+data.set_index([data.index, 'whoAmI'], inplace=True)
+data = data.unstack(level=-1, fill_value = 0).astype(int)
+data.columns = data.columns.droplevel()
+data.columns.name = None
+print(data)
+
